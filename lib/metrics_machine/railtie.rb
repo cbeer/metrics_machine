c982fba05907826125526220be5336dbab7e7de0
Chris Beer <cabeer@stanford.edu>
2013-08-13 16:03:12 -0700

    only auto-wire our initializers in production

--- lib/metrics_machine/railtie.rb
+++ lib/metrics_machine/railtie.rb
@@ -1,15 +1,17 @@
 module MetricsMachine
   class Railtie < Rails::Railtie
 
-    initializer "metricsmachine.start_em" do
-      unless EM.reactor_running?
-        MetricsMachine.start
+    if Rails.env.production?
+      initializer "metricsmachine.start_em" do
+        unless EM.reactor_running?
+          MetricsMachine.start
+        end
       end
-    end
 
-    initializer "metricsmachine.configure_rails_initialization" do
-      MetricsMachine.configure do
-        monitor :mysql, ActiveRecord::Base.connection if ActiveRecord::Base.connection.is_a? ActiveRecord::ConnectionAdapters::Mysql2Adapter
+      initializer "metricsmachine.configure_rails_initialization" do
+        MetricsMachine.configure do
+          monitor :mysql, ActiveRecord::Base.connection if ActiveRecord::Base.connection.is_a? ActiveRecord::ConnectionAdapters::Mysql2Adapter
+        end
       end
     end
 
